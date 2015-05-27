99b77279c5e760970a0bd822639667879c7f0055
Ilya Uvarenkov <trukanduk@gmail.com>
2015-05-28 02:06:22 +0300

    tabs -> spaces (build fix)

--- src/Main-nowx.hs
+++ src/Main-nowx.hs
@@ -5,15 +5,17 @@ import PlayerConsole
 import DrawingConsole
 import Controller
 import AIPlayer
+
 main :: IO ()
 main = do
-		winner <- run cfg player1 player2 [drawing]
-		case winner of
-	    	Winner color -> putStrLn $ (show color) ++ " player wins!"
-	    	DrawBy color -> putStrLn $ "It's a trap for " ++ (show color) ++ " player!"
-	where
-		cfg = defaultConfig
-		game = createGame cfg
-		player1 = createAIPlayer 1 2
-		player2 = createPlayerConsole
-		drawing = createDrawingConsole
+    winner <- run cfg player1 player2 [drawing]
+    case winner of
+      Winner color -> putStrLn $ (show color) ++ " player wins!"
+      DrawBy color -> putStrLn $ "It's a trap for " ++ (show color) ++ " player!"
+    return ()
+  where
+    cfg = defaultConfig
+    game = createGame cfg
+    player1 = createAIPlayer 1 2
+    player2 = createPlayerConsole
+    drawing = createDrawingConsole
